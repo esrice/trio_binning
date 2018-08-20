892210d3a7ffc2fca9f81c93ca10956c57881427
Edward S. Rice <esrice@soe.ucsc.edu>
2018-08-20 16:35:45 -0700

    Added a generic Record/Reader
    These just act as wrappers for Records and Readers for specific file
    types containing sequences.

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,9 @@
 extern crate trio_binning;
 
-use trio_binning::seq::{fasta, fastq};
-use std::fs::File;
+use trio_binning::seq::{self, fastq};
 
 fn main() {
-    let reader = fasta::Reader::new(File::open("test.fa").unwrap());
+    let reader = seq::Reader::from_filename("test.fa").unwrap();
 
     for result in reader {
         let read = result.unwrap();
@@ -16,33 +15,10 @@ fn main() {
         Err(e) => eprintln!("ERROR: {}", e),
     }
 
-    let s = "ABCDEFG\nABCDEFGsdf";
-    let lines: Vec<&str> = s.split('\n').collect();
-    println!("{:?}", lines);
-
-    let bad_reader = fasta::Reader::new(File::open("test_bad.fa").unwrap());
-    for result in bad_reader {
-        match result {
-            Ok(r) => 
-                panic!("Should not get Ok for a misformatted fasta file! {:?}",
-                       r),
-            Err(e) => eprintln!("test_bad.fa: {}", e),
-        }
-    }
-
-    let fastq_reader = fastq::Reader::new(File::open("test.fastq").unwrap());
-
-    for result in fastq_reader {
-        let read = result.unwrap();
-        println!("ID:{}\tSEQ:{}\tQUAL:{}", read.id(), read.seq(), read.qual());
-    }
-
-    let mut bad_fq_reader = fastq::Reader::new(
-        File::open("test_bad.fastq").unwrap());
-    let good_read = bad_fq_reader.next().unwrap().unwrap();
-    println!("ID:{}\tSEQ:{}\tQUAL:{}", good_read.id(), good_read.seq(),
-            good_read.qual());
-    match bad_fq_reader.next().unwrap() {
+    let mut bad_reader = seq::Reader::from_filename("test_bad.fastq").unwrap();
+    let good_read = bad_reader.next().unwrap().unwrap();
+    println!("ID:{}\tSEQ:{}", good_read.id(), good_read.seq());
+    match bad_reader.next().unwrap() {
         Ok(_) => panic!("This read should not be Ok!"),
         Err(e) => eprintln!("test_bad.fastq: {}", e),
     };
--- src/seq/mod.rs
+++ src/seq/mod.rs
@@ -1,2 +1,105 @@
+use std::fmt;
+use std::path::Path;
+use std::fs::File;
+use std::io::{self, Read};
+use std::result;
+use std::error;
+
 pub mod fasta;
 pub mod fastq;
+
+/// wrapper for various errors that can occur here
+#[derive(Debug)]
+pub enum ReaderError {
+    Fasta(fasta::FastaError), // errors coming from the fasta module
+    Fastq(fastq::FastqError), // errors coming from the fastq module
+    Io(io::Error), // errors coming from the io module
+    Other(String), // errors generated in this module
+}
+
+impl fmt::Display for ReaderError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            ReaderError::Fasta(e) => write!(f, "{}", e),
+            ReaderError::Fastq(e) => write!(f, "{}", e),
+            ReaderError::Io(e) => write!(f, "{}", e),
+            ReaderError::Other(s) => write!(f, "{}", s),
+        }
+    }
+}
+
+impl error::Error for ReaderError {}
+
+type Result<T> = result::Result<T, ReaderError>;
+
+/// wrapper for records containing an ID and a sequence, so that the user does
+/// not need to worry about what kind of file is being read, just that it
+/// contains sequences with IDs
+pub enum Record {
+    Fasta(fasta::Record),
+    Fastq(fastq::Record),
+}
+
+impl Record {
+    pub fn id(&self) -> &str {
+        match self {
+            Record::Fasta(r) => r.id(),
+            Record::Fastq(r) => r.id(),
+        }
+    }
+
+    pub fn seq(&self) -> &str {
+        match self {
+            Record::Fasta(r) => r.seq(),
+            Record::Fastq(r) => r.seq(),
+        }
+    }
+}
+
+impl fmt::Display for Record {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Record::Fasta(r) => write!(f, "{}", r.to_string()),
+            Record::Fastq(r) => write!(f, "{}", r.to_string()),
+        }
+    }
+}
+
+/// wrapper for various file types containing sets of sequences, so that the
+/// user can just call next() and get generic Records back without worrying
+/// about what kind of file is being parsed
+pub enum Reader<T> {
+    Fasta(fasta::Reader<T>),
+    Fastq(fastq::Reader<T>),
+}
+
+impl Reader<File> {
+    pub fn from_filename(filename: &str) -> Result<Reader<File>> {
+        let extension = Path::new(filename).extension()
+            .and_then(|e| e.to_str())
+            .ok_or(ReaderError::Other("Filename has no extension!"
+                                         .to_owned()))?;
+        let file = File::open(filename).map_err(|e| ReaderError::Io(e))?;
+
+        match extension {
+            "fasta" | "fa" => Ok(Reader::Fasta(fasta::Reader::new(file))),
+            "fastq" | "fq" => Ok(Reader::Fastq(fastq::Reader::new(file))),
+            _ => Err(ReaderError::Other(
+                    "Do not recognize filename.".to_owned())),
+        }
+    }
+}
+
+impl<T: Read> Iterator for Reader<T> {
+    type Item = Result<Record>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        match self {
+            Reader::Fasta(r) => r.next().map(|s| s.map(|t| Record::Fasta(t))
+                .map_err(|e| ReaderError::Fasta(e))),
+            Reader::Fastq(r) => r.next().map(|s| s.map(|t| Record::Fastq(t))
+                .map_err(|e| ReaderError::Fastq(e))),
+        }
+    }
+}
+
