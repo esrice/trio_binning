5ce9e8d3dccf49037d0070691274899d0f1fc0c4
Edward S. Rice <edward.rice@missouri.edu>
2022-09-03 13:50:03 -0700

    Add a proof-of-concept of python-c interface

---
+++ trio_binning/kmers_c_test.py
@@ -0,0 +1,46 @@
+from ctypes import cdll, Structure, c_int, c_ubyte, POINTER, c_uint64, c_char_p, byref
+
+class HashSet(Structure):
+    _fields_: list = [
+        ("kmers", POINTER(c_uint64)),
+        ("full", POINTER(c_ubyte)),
+        ("hash_size", c_int),
+        ("k", c_ubyte),
+        ("num_kmers", c_int),
+    ]
+
+def main():
+    lib = cdll.LoadLibrary("../c/kmers.so")
+
+    create_kmer_hash_set = lib.create_kmer_hash_set
+    create_kmer_hash_set.argtypes = [c_char_p]
+    create_kmer_hash_set.restype = POINTER(HashSet)
+
+    count_kmers_in_read = lib.count_kmers_in_read
+    count_kmers_in_read.argtypes = [
+        c_char_p,
+        c_int,
+        POINTER(HashSet),
+        POINTER(HashSet),
+        POINTER(c_int),
+        POINTER(c_int),
+    ]
+
+    hapA = create_kmer_hash_set(b"../c/hapA.txt")
+    hapB = create_kmer_hash_set(b"../c/hapB.txt")
+
+    count_a, count_b = c_int(), c_int()
+
+    count_kmers_in_read(
+        b"GAGGAGATTTAGAGTGTGAGTCGAGCATAGAGATATATA",
+        c_int(39),
+        hapA,
+        hapB,
+        byref(count_a),
+        byref(count_b),
+    )
+
+    print(f"Found {count_a.value} kmers from hapA and {count_b.value} from hapB in read.")
+
+if __name__ == "__main__":
+    main()
